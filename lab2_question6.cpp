c239116d87d86cc03744fd79eb9c9efaaea9fcd9
SmrutimedhaParida <31158818+SmrutimedhaParida@users.noreply.github.com>
2017-08-28 17:23:25 +0530

    Create lab2_question6.cpp

---
+++ lab2_question6.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main ()
+{
+double  number,root;
+cout<<"Enter a number";
+cin>>number;
+root = pow(number,0.5);
+cout<<"the square root of the number is:"<< root << "\n";
+return 0;
+}
