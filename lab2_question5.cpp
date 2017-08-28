a27894baf92abc1d6047aa4e74dde5af679e9a91
SmrutimedhaParida <31158818+SmrutimedhaParida@users.noreply.github.com>
2017-08-28 17:09:09 +0530

    Create lab2_question5.cpp

---
+++ lab2_question5.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main ()
+{
+int x,y,z;
+cout<<"enter a base and exponent:";
+cin >> x >> y;
+z = pow(x,y);
+cout<<"the required answer is:"<< z << "\n";
+return 0;
+}
