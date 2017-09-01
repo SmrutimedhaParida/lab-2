40a8084b43e999a9b08dae7b0f9004b64c3e8a03
SmrutimedhaParida <31158818+SmrutimedhaParida@users.noreply.github.com>
2017-09-01 19:52:12 +0530

    Create lab2_question9.cpp

---
+++ lab2_question9.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+double l, area;
+cout << "enter length of any side:\n";
+cin >> l;
+area=(1.732/4)*l*l;
+cout << "the area of the equilateral triangle is = "<<area<<"\n";
+return 0;
+}
