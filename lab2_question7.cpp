2f311c3402bf9aba200986ac1b45de32621bc720
SmrutimedhaParida <31158818+SmrutimedhaParida@users.noreply.github.com>
2017-09-01 19:32:45 +0530

    Create lab2_question7.cpp

---
+++ lab2_question7.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+double a, b, c;
+cout << "enter two angles in degrees\n";
+cin >> a >> b;
+c=180-(a+b);
+cout << "the third angle is = "<<c<<"\n";
+return 0;
+}
