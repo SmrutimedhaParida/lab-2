90966c8a938163ef1b0239b4993011d72ee7fa98
SmrutimedhaParida <31158818+SmrutimedhaParida@users.noreply.github.com>
2017-09-01 19:42:01 +0530

    Create lab2_question8.cpp

---
+++ lab2_question8.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+double height, base, area;
+cout << "enter height and base\n";
+cin >> height >> base;
+area=0.5*height*base;
+cout << "the area of the triangle is = "<<area<<"\n";
+return 0;
+}
